614177acb6d899dd2cd105586e968797c2ade4d1
angel.castro2 <angel.castro2@udc.es>
2015-11-08 13:20:43 +0100

    Implementación de canción casi finalizada

--- src/main/java/Contenido/Cancion.java
+++ src/main/java/Contenido/Cancion.java
@@ -6,40 +6,46 @@ public class Cancion implements Contenido {
 
 	private String titulo;
 	private int duracion;
+	private List<Contenido> listaReproduccion;
 	
 	public Cancion(String titulo, int duracion){
 		this.titulo = titulo;
 		this.duracion = duracion;
 	}
 
+	//Obtiene el titulo
 	public String obtenerTitulo() {
-		
 		return titulo;
 	}
-
+	
+	//Obtiene la duración
 	public int obtenerDuracion() {
-		
 		return duracion;
 	}
 
+	//Obtiene la lista de reproducción
 	public List<Contenido> obtenerListaReproduccion() {
-		// TODO Auto-generated method stub
-		return null;
+		return listaReproduccion;
 	}
 
+	/* AQUI TENGO DUDAS
+	 * */
 	public List<Contenido> buscar(String subcadena) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * AQUI TENGO DUDAS, falta acabar esto
+		 */
 		return null;
 	}
-
+	
+	//Canción no implementa esta funcionalidad
 	public void agregar(Contenido contenido, Contenido predecesor) {
-		// TODO Auto-generated method stub
-		
+		return;
 	}
 
+	//Canción no implementa esta funcionalidad
 	public void eliminar(Contenido contenido) {
-		// TODO Auto-generated method stub
-		
+		return;
 	}
 
 
