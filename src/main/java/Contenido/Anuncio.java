afdbda67499e934697533b7a079d65046b76c621
avelino.rios <avelino.rios@udc.es>
2015-11-08 11:43:18 +0100

    Commit con carpetas que os faltan

--- src/main/java/Contenido/Anuncio.java
+++ src/main/java/Contenido/Anuncio.java
@@ -1,5 +1,47 @@
 package Contenido;
 
-public class Anuncio {
+import java.util.List;
 
+public class Anuncio implements Contenido {
+
+	private String titulo = "PUBLICIDAD";
+	private int duracion = 5;
+	
+	public Anuncio() {	}
+
+
+	public String obtenerTitulo() {
+		
+		return titulo;
+	}
+
+	public int obtenerDuracion() {
+
+		return duracion;
+	}
+
+	public List<Contenido> obtenerListaReproduccion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Contenido> buscar(String elemento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	public void agregar(Contenido contenido, Contenido predecesor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void eliminar(Contenido contenido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- src/main/java/Contenido/Cancion.java
+++ src/main/java/Contenido/Cancion.java
@@ -1,5 +1,48 @@
 package Contenido;
 
-public class Cancion {
-//Probando
+import java.util.List;
+
+public class Cancion implements Contenido {
+
+	private String titulo;
+	private int duracion;
+	
+	public Cancion(String titulo, int duracion){
+		this.titulo = titulo;
+		this.duracion = duracion;
+	}
+
+	public String obtenerTitulo() {
+		
+		return titulo;
+	}
+
+	public int obtenerDuracion() {
+		
+		return duracion;
+	}
+
+	public List<Contenido> obtenerListaReproduccion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Contenido> buscar(String subcadena) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void agregar(Contenido contenido, Contenido predecesor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void eliminar(Contenido contenido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	
 }
