aee0381a5058a4a01b681ea66c738e9aa1cbb289
angel.castro2 <angel.castro2@udc.es>
2015-11-08 13:00:48 +0100

    Emisora finalizada con comentarios en todos los metodos

--- src/main/java/Contenido/Emisora.java
+++ src/main/java/Contenido/Emisora.java
@@ -9,25 +9,28 @@ public class Emisora implements Contenido {
 	private int duracion;
 	private List<Contenido> listaReproduccion;
 	
-	
+	//Constructor 
 	public Emisora(String titulo, int duracion){
 		this.titulo=titulo;
 		this.duracion=duracion;
 	}
 	
-	
+	//Obtiene el titulo
 	public String obtenerTitulo() {
 		return titulo;
 	}
-
+	//Obtiene la duración
 	public int obtenerDuracion() {
 		return duracion;
 	}
-
+	//Obtiene la lista de reproducción
 	public List<Contenido> obtenerListaReproduccion() {
 		return listaReproduccion;
 	}
-
+	/*Busca la subcadena pasada pro parámetro en el titulo de los elementos
+	 * de la lista de  reproducción, si no encuentra ada entonces devuelve
+	 * lista vacía
+	 */
 	public List<Contenido> buscar(String subcadena) {
 		List<Contenido> resultado = new ArrayList<Contenido>();
 		for(int i=0; i < listaReproduccion.size();i++){
@@ -38,11 +41,16 @@ public class Emisora implements Contenido {
 		return resultado;
 	}
 
+	/*Busca el predecesor e inserta el contenido después del predecesor en 
+	 * la lista de reproducción
+	 * */
 	public void agregar(Contenido contenido, Contenido predecesor) {
-		// TODO Auto-generated method stub
+		listaReproduccion.add(listaReproduccion.indexOf(predecesor) + 1 , contenido);
 		
 	}
-
+	/*Elimina el contenido pasado de la emisora 
+	 *(solo la primera aparicion en la lista)
+	 */
 	public void eliminar(Contenido contenido) {
 		listaReproduccion.remove(contenido);
 		
