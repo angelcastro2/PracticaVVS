5ae7e8c2f28fc25b9041f7c5a08b7e1ffacce6ab
angel.castro2 <angel.castro2@udc.es>
2015-12-12 18:18:55 +0100

    graphwalker en proceso, faltan test solo

--- src/test/java/GraphWalker/GraphWalkerTest.java
+++ src/test/java/GraphWalker/GraphWalkerTest.java
@@ -1,6 +1,42 @@
 package GraphWalker;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.graphwalker.core.machine.ExecutionContext;
 
-public class GraphWalkerTest {
+
+public class GraphWalkerTest extends ExecutionContext implements grafica {
+
+    public final static Path GRAFICA_PATH = Paths.get("main/resources/model/grafica.graphml");
+    
+    @Override
+    public void inicializarServidor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void borrar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Inicio() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buscar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void agregar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ServidorConContenidos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
