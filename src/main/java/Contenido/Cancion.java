5d365df06892772db369a4f695b25c9c5360bfac
avelino.rios <avelino.rios@udc.es>
2015-12-13 16:50:20 +0100

    FindBugs, PMD y CheckStyle pasados en la carpeta Contenido

--- src/main/java/Contenido/Cancion.java
+++ src/main/java/Contenido/Cancion.java
@@ -5,55 +5,65 @@ import java.util.List;
 
 public class Cancion implements Contenido {
 
-	private String titulo;
-	private int duracion;
-	
-	public Cancion(String titulo, int duracion){
-		this.titulo = titulo;
-		this.duracion = duracion;
-	}
+    private String titulo;
+    private int duracion;
 
-	
-	public String obtenerTitulo() {
-		return titulo;
-	}
-	
-	
-	public int obtenerDuracion() {
-		return duracion;
-	}
+    public Cancion(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
 
-	
-	public List<Contenido> obtenerListaReproduccion() {
-		List<Contenido> listaResultado= new ArrayList<Contenido>();
-		listaResultado.add(this);
-		
-		return listaResultado;
-	}
+    public String obtenerTitulo() {
+        return titulo;
+    }
 
-	public List<Contenido> buscar(String subcadena) {
-            List<Contenido> listaResultado= new ArrayList<Contenido>();
-            if (titulo.toLowerCase().contains(subcadena.toLowerCase())){
-		listaResultado.add(this);
-            }
-            return listaResultado;
-	}
-	
-	/*
+    public int obtenerDuracion() {
+        return duracion;
+    }
+
+    public List<Contenido> obtenerListaReproduccion() {
+        List<Contenido> listaResultado = new ArrayList<Contenido>();
+        listaResultado.add(this);
+
+        return listaResultado;
+    }
+
+    public List<Contenido> buscar(String subcadena) {
+        List<Contenido> listaResultado = new ArrayList<Contenido>();
+        if (titulo.toLowerCase().contains(subcadena.toLowerCase())) {
+            listaResultado.add(this);
+        }
+        return listaResultado;
+    }
+
+    /*
 	 * Método no implementado por Cancion.
-	 */
-	public void agregar(Contenido contenido, Contenido predecesor) {
-		return;
-	}
+     */
+    public void agregar(Contenido contenido, Contenido predecesor) {
+        return;
+    }
 
-	/*
+    /*
 	 * Método no implementado por Cancion.
-	 */
-	public void eliminar(Contenido contenido) {
-		return;
-	}
+     */
+    public void eliminar(Contenido contenido) {
+        return;
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
 
-	
-	
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
 }
--- src/main/java/Contenido/Emisora.java
+++ src/main/java/Contenido/Emisora.java
@@ -7,64 +7,83 @@ import javax.management.InstanceNotFoundException;
 
 public class Emisora implements Contenido {
 
-	private String titulo;
-	private int duracion;
-	private List<Contenido> listaReproduccion;
-	
-	//Constructor 
-	public Emisora(String titulo){
-		this.titulo=titulo;
-		this.duracion=0;
-		listaReproduccion = new ArrayList<Contenido>();
-	}
-	
-	
-	public String obtenerTitulo() {
-		return titulo;
-	}
-	
-	public int obtenerDuracion() {
-		return duracion;
-	}
-	
-	public List<Contenido> obtenerListaReproduccion() {
-		return listaReproduccion;
-	}
-	
-	
-	public List<Contenido> buscar(String subcadena) {
-		List<Contenido> resultado = new ArrayList<Contenido>();
-		for(Contenido c: listaReproduccion){
-			if (c.obtenerTitulo().contains(subcadena)){
-				resultado.add(c);
-			}
-		}
-		return resultado;
-	}
+    private String titulo;
+    private int duracion;
+    private List<Contenido> listaReproduccion;
 
-	
-	
-	public void agregar(Contenido contenido, Contenido predecesor) {
-		listaReproduccion.add((listaReproduccion.indexOf(predecesor) + 1) , contenido);
-		duracion+=contenido.obtenerDuracion();
-		
-	}
+    //Constructor 
+    public Emisora(String titulo) {
+        this.titulo = titulo;
+        this.duracion = 0;
+        listaReproduccion = new ArrayList<Contenido>();
+    }
 
-	public void eliminar(Contenido contenido) throws InstanceNotFoundException {
-		int size = 0;
-		for (int i=0; i<listaReproduccion.size(); i++){
-			if (listaReproduccion.get(i).equals(contenido)){
-				size++;
-			}
-		}
-		if(size>0){
-			duracion = duracion-contenido.obtenerDuracion();
-			listaReproduccion.remove(contenido);
-		} else {
-			throw new InstanceNotFoundException();
-		}
-		
-		
-	}
+    public String obtenerTitulo() {
+        return titulo;
+    }
+
+    public int obtenerDuracion() {
+        return duracion;
+    }
+
+    public List<Contenido> obtenerListaReproduccion() {
+        return listaReproduccion;
+    }
+
+    public List<Contenido> buscar(String subcadena) {
+        List<Contenido> resultado = new ArrayList<Contenido>();
+        for (Contenido c : listaReproduccion) {
+            if (c.obtenerTitulo().contains(subcadena)) {
+                resultado.add(c);
+            }
+        }
+        return resultado;
+    }
+
+    public void agregar(Contenido contenido, Contenido predecesor) {
+        listaReproduccion.add((listaReproduccion.indexOf(predecesor) + 1), contenido);
+        duracion += contenido.obtenerDuracion();
+
+    }
+
+    public void eliminar(Contenido contenido) throws InstanceNotFoundException {
+        int size = 0;
+        for (int i = 0; i < listaReproduccion.size(); i++) {
+            if (listaReproduccion.get(i).equals(contenido)) {
+                size++;
+            }
+        }
+        if (size > 0) {
+            duracion = duracion - contenido.obtenerDuracion();
+            listaReproduccion.remove(contenido);
+        } else {
+            throw new InstanceNotFoundException();
+        }
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public List<Contenido> getListaReproduccion() {
+        return listaReproduccion;
+    }
+
+    public void setListaReproduccion(List<Contenido> listaReproduccion) {
+        this.listaReproduccion = listaReproduccion;
+    }
 
 }
